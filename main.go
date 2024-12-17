7c0b004e90615ee58b0997e5044f626bba85c274
Meenakshi Gupta <guptameenakshi957@gmail.com>
2024-12-17 22:52:00 +0530

    added validation to inputs

--- main.go
+++ main.go
@@ -9,18 +9,19 @@ func main() {
 	// Conference details
 	var conferenceName = "GoConf"
 	const totalTickets = 100
-	var remainingTickets uint= 100
+	var remainingTickets uint = 100
 	var bookedTickets = 0
 
 	// Slice to store bookings dynamically
 	var bookingsList []string
-
+    
 	fmt.Printf("Welcome to the %v booking application!\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets, and %v tickets are currently available.\n", totalTickets, remainingTickets)
 	fmt.Println("Book your tickets now!")
 
 	// Start the booking process
-	for {
+	for remainingTickets > 0 && len(bookingsList) < 50 {
+		fmt.Println("------------------------------------------------------------")
 		fmt.Printf("Currently, %v tickets are booked, and %v tickets are remaining.\n", bookedTickets, remainingTickets)
 
 		// Collect user details
@@ -36,10 +37,31 @@ func main() {
 		fmt.Print("Enter the number of tickets you'd like to book: ")
 		fmt.Scan(&ticketsToBook)
 
+		//validate name
+		isValidName := len(firstName) > 2 && len(lastName) > 2
+		if !isValidName {
+			fmt.Println("Please enter name correctly with atleast 2 characters")
+			continue
+		}
+
+		//validate email address
+		isValidEmail := strings.Contains(email, "@gmail.com")
+
+		if !isValidEmail {
+			fmt.Println("Please enter a valid email address")
+			continue
+		}
+        
+		//valid tickets
+		isValidTickets := ticketsToBook>0
+		if !isValidTickets{
+			fmt.Println("Enter positive number of tickets")
+		}
+
 		// Check ticket availability
 		if ticketsToBook > remainingTickets {
 			fmt.Printf("Sorry, we only have %v tickets left. Please try again.\n", remainingTickets)
-			fmt.Println("------------------------------------------------------------")
+			
 			continue
 		}
 
@@ -65,7 +87,6 @@ func main() {
 			firstNames = append(firstNames, firstName)
 		}
 		fmt.Printf("First names of all attendees: %v\n", firstNames)
-		fmt.Println("------------------------------------------------------------")
 
 		// End booking if no tickets are left
 		if remainingTickets == 0 {
